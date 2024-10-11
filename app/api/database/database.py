f91288ab6ca004e04a584856b11bf446aaf1d11b
Diego <diego.gerardo95@hotmail.com>
2024-10-11 19:38:56 -0300

    set up database connection

---
+++ app/api/database/database.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import OperationalError
+import os
+
+load_dotenv()
+
+url_database = os.getenv('URL_DATABASE')
+
+engine = create_engine(url_database)    
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except OperationalError as e:
+        
+        print('Base de datos no conectada.')
+
+    finally:
+        db.close()
+
