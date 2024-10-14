d6ff0e61c6e236b115bfdd995df29d51d7418ec9
Diego <diego.gerardo95@hotmail.com>
2024-10-14 13:41:24 -0300

    Updated include router rout  to match new versionated api structure

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, status
-from app.api.classes.users.routers import user_router
+from app.api.v1.classes.users.routers import user_router
 
 app = FastAPI()
 
 
-app.include_router(user_router, prefix='/api/users', tags=['Users'])
+app.include_router(user_router, prefix='/api/v1/users', tags=['Users'])
 
 
 @app.get('/', status_code=status.HTTP_200_OK)
