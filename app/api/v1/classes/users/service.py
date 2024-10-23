1c9e0722a29b4af9544a786cb5b7a7e7c82103f0
Diego <diego.gerardo95@hotmail.com>
2024-10-23 11:14:49 -0300

    build connection between orm sqlalchemy models and pydanctic models

--- app/api/v1/classes/users/service.py
+++ app/api/v1/classes/users/service.py
@@ -1,5 +1,6 @@
-from app.api.v1.classes.users.schemas import UserRegister
+from app.api.v1.classes.users.schemas import UserRegister, UserInfo
 from app.api.v1.database.database import get_db
+from app.api.v1.classes.users.models import User
 from fastapi import Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
@@ -10,24 +11,27 @@ import bcrypt
 
 def register_user(user: UserRegister, db: Session):
 
-    #Faltaria buscar si no existe ya un email registrado!
-
-    hashed_password = bcrypt.hashpw(user.hashed_password.encode('utf-8') , bcrypt.gensalt())
-    user.hashed_password = hashed_password
+    sql_user: User = db.query(User).filter(User.email == user.email).first()
+    if sql_user:
+        raise ValueError('User already registered.')
+    
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8') , bcrypt.gensalt())
+    user.password = hashed_password
     
     new_user: User = User(
-    hashed_password = user.hashed_password, 
+    hashed_password = user.password, 
     name = user.name,
     lastname =user.lastname,
     email = user.email,
     birthdate = user.birthdate,
-     id_user= uuid4().bytes
+     id_user= uuid4().bytes,
+     warning_amounts = 0
     , rented_books=0
-    , user_role=Role.user)
+    , user_role=Role.user.value)
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-
-    return user
+    #user_info : UserInfo = UserInfo( **{k: v for k, v in new_user.__dict__.items() if k in UserInfo.model_fields})
+    return new_user
 
