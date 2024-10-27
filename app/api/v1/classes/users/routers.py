6396c69ecf2b3c76a874fa890ac8214ba6341461
Diego <diego.gerardo95@hotmail.com>
2024-10-27 00:29:53 -0300

    Add get all users route

--- app/api/v1/classes/users/routers.py
+++ app/api/v1/classes/users/routers.py
@@ -13,27 +13,27 @@ user_router = APIRouter()
 #<----------- GET ----------->#
 
 '[GET] Retorna la Home que prueba el enrutamiento (?)'
-@user_router.get('/', status_code=status.HTTP_200_OK)
-def home_user():
-    return 'USERS.'
+@user_router.get('/', response_model=list[UserInfo],status_code=status.HTTP_200_OK)
+async def home_user(db: Session = Depends(get_db)):
+    return service.get_all_users(db)
 
 '[GET] Devuelve la informacion del usuario por su id' #PROTEGER
 @user_router.get('/{id}', response_model=UserInfo ,status_code=status.HTTP_200_OK)
-def get_one_user(id: UUID):
-    pass
-    #Llamada al servicio que se dirige a la base de datos a buscar la info del usuario con el UUID correspondiente.
+async def get_one_user(id: UUID, db: Session = Depends(get_db)):
+    return service.get_user_info(id, db)
+   
 
-
-'[GET] Devuelve todos los usuarios para listado en seccion admin' #PROTEGER
-@user_router.get('/', response_model=list[UserInfo], status_code=status.HTTP_200_OK)
-def get_all_users():
-    pass
+'''Tener cuidado con rutas como '/all' ya que toma como que 'all' es un id! Ver ejemplo abajo.'''
+# '[GET] Devuelve todos los usuarios para listado en seccion admin' #PROTEGER
+# @user_router.get('/all', response_model=list[UserInfo], status_code=status.HTTP_200_OK)
+# async def get_all_users(db:Session=Depends(get_db)):
+#     return service.get_all_users(db)
 
 #<----------- POST ----------->#
 
 '[POST] Envia a la base de datos la informacion del usuario a crear' 
 @user_router.post('/register', status_code=status.HTTP_201_CREATED, response_model= UserInfo) #PROTEGER
-def register(user_register: UserRegister, db: Session = Depends(get_db)):
+async def register(user_register: UserRegister, db: Session = Depends(get_db)):
     
     try:
         return service.register_user(user_register,db)
