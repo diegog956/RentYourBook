c4cd93641915c7874b31d87a4f4bee1c7ef55a43
Diego <diego.gerardo95@hotmail.com>
2024-11-06 12:10:29 -0300

    add login router

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, status
 from app.api.v1.classes.users.routers import user_router
+from app.api.v1.auth.authentication import login_router
 
 app = FastAPI()
 
 
 app.include_router(user_router, prefix='/api/v1/users', tags=['Users'])
-
+app.include_router(login_router, prefix='/api/v1', tags=['Login'])
 
 @app.get('/', status_code=status.HTTP_200_OK)
 def home():
