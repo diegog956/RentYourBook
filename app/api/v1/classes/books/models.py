51acb3e54e977ab6f45db4681a62bcccf75bfbf7
Diego <diego.gerardo95@hotmail.com>
2024-11-19 11:10:13 -0300

    sqlalchmey models for books as literal works

---
+++ app/api/v1/classes/books/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, Date
+
+Base = declarative_base()
+
+
+class Books(Base):
+
+    __tablename__ = 'Books'
+    id_book: int = Column(Integer, primary_key=True, autoincrement=True)
+    title: str = Column(String, nullable=False)
+    release_year: Date = Column(Date, nullable=False)
+    favorites_amount:int = Column(Integer, default=0 ,nullable=False)
+    available_units: int = Column(Integer, default=1, nullable=False)
+
+    #Añadir Relationships entre tablas.
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, status
 from app.api.v1.classes.users.routers import user_router
 from app.api.v1.auth.authentication import login_router
+from app.api.v1.classes.books.routers import book_router
 
 app = FastAPI()
 
 
 app.include_router(user_router, prefix='/api/v1/users', tags=['Users'])
 app.include_router(login_router, prefix='/api/v1', tags=['Login'])
+app.include_router(book_router, prefix='/api/v1/books', tags=['Books'])
 
 @app.get('/', status_code=status.HTTP_200_OK)
 def home():
